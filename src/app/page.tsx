62dbd04fc408a2144cb04a5d3ae3bf48718d12b6
AdLap <adamlapacz@gmail.com>
2024-02-07 01:05:05 +0100

    move products to utils, eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,5 @@
 import { ProductList } from "@/ui/molecules/ProductList";
-
-const products = [
-	{
-		name: "Metro",
-		description: "Stacje metra",
-		category: "Podróże",
-		price: 100.99,
-		image: {
-			src: "https://picsum.photos/id/345/250/300",
-			alt: "Product 1",
-		},
-	},
-	{
-		name: "Tory",
-		description: "Objazdówka pociągiem",
-		category: "Podróże",
-		price: 290.77,
-		image: {
-			src: "https://picsum.photos/id/233/250/300",
-			alt: "Product 2",
-		},
-	},
-	{
-		name: "Pole",
-		description: "Dożynki",
-		category: "Wycieczki",
-		price: 3000,
-		image: {
-			src: "https://picsum.photos/id/98/250/300",
-			alt: "Product 3",
-		},
-	},
-	{
-		name: "Plaża",
-		description: "Utopce",
-		category: "Wycieczki",
-		price: 630.07,
-		image: {
-			src: "https://picsum.photos/id/100/250/300",
-			alt: "Product 4",
-		},
-	},
-	{
-		name: "Most",
-		description: "Oszukać przeznaczenie",
-		category: "Podróże",
-		price: 547.0,
-		image: {
-			src: "https://picsum.photos/id/84/250/300",
-			alt: "Product 5",
-		},
-	},
-	{
-		name: "Szpilki",
-		description: "Takie tam gadżety",
-		category: "Dodatki",
-		price: 89.4,
-		image: {
-			src: "https://picsum.photos/id/21/250/300",
-			alt: "Product 6",
-		},
-	},
-];
+import { products } from "@/utils/products-list";
 
 export default function Home() {
 	return (
@@ -70,5 +8,3 @@ export default function Home() {
 		</main>
 	);
 }
-
-// flex min-h-screen flex-col items-center justify-between p-24
