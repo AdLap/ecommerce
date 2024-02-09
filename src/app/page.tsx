aa2771d717a08a9c9331d717698723fcc831830f
AdLap <adamlapacz@gmail.com>
2024-02-09 02:25:45 +0100

    Added Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { ProductList } from "@/organisms/ProductList";
+import { Header } from "@/organisms/header/Header";
+import { ProductList } from "@/organisms/product/ProductList";
 import { products } from "@/utils/products-list";
 
 export default function Home() {
 	return (
-		<main className="mx-auto min-h-screen w-full p-4 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+		<main className="mx-auto min-h-screen w-full px-4 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+			<Header />
 			<ProductList products={products} />
 		</main>
 	);
 }
-
-//flex min-h-screen flex-col items-center justify-between p-24
