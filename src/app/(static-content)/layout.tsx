6c7b50867234cbb0ad260b9f96dddbbe3b35c794
AdLap <adamlapacz@gmail.com>
2024-02-09 20:21:24 +0100

    group

--- src/app/(static-content)/layout.tsx
+++ src/app/(static-content)/layout.tsx
@@ -1,8 +1,3 @@
 export default function StaticLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="text-center max-w-md mx-auto">
-      {children}
-    </div>
-
-  )
+	return <div className="mx-auto max-w-md text-center">{children}</div>;
 }
---
+++ src/app/(with-navigation)/layout.tsx
@@ -0,0 +1,3 @@
+export default function WithNavigationLayout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>;
+}
