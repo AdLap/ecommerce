8c9ee7927af88cbf93c9cd1146a5e8880c9d6f89
AdLap <adamlapacz@gmail.com>
2024-02-13 18:12:14 +0100

    added static params

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
-import { getProductById } from "@/api/products";
+import { getAllProducts, getProductById } from "@/api/products";
 import { ProductCardItem } from "@/ui/molecules/product/card/ProductCardItem";
+import { type ProductItem } from "@/ui/types/types";
 
 export type ProductPageProps = {
 	params: {
@@ -7,6 +8,13 @@ export type ProductPageProps = {
 	};
 	searchParams: Record<string, string | string[]>;
 };
+
+export const generateStaticParams = async (): Promise<{productId: string}[]> => {
+	const products = await getAllProducts(1);
+	return products.map((product: ProductItem) => ({
+		productId: product.id,
+	})).slice(0, 3);
+}
 
 export default async function ProductPage({ params }: ProductPageProps) {
 	const product = await getProductById(params.productId);
