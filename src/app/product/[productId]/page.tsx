c3d0f93cf0968c8690424e3be4e4f1fbedd86296
AdLap <adamlapacz@gmail.com>
2024-02-13 03:08:52 +0100

    layout fix

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -12,7 +12,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
 	const product = await getProductById(params.productId);
 
 	return (
-		<section className="mx-auto max-w-3xl">
+		<section className="mx-auto max-w-3xl pt-32">
 			<h1 className="mb-4 text-4xl font-bold">{product?.name}</h1>
 			{product !== undefined ? <ProductCardItem {...product} /> : <p>Product not found</p>}
 		</section>
