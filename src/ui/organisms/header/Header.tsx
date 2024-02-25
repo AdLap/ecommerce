c933733088d24146d94f34c9b3f034701773943a
AdLap <adamlapacz@gmail.com>
2024-02-25 04:10:45 +0100

    fix - routes.path

--- src/ui/organisms/header/Header.tsx
+++ src/ui/organisms/header/Header.tsx
@@ -6,9 +6,9 @@ export const Header = () => {
 	const routes: INavItem[] = [
 		{ path: '/', name: 'Home' },
 		{ path: 'products', name: 'All' },
-		{ path: 'category/t-shirts/1', name: 'T-Shirts' },
-		{ path: 'category/hoodies/1', name: 'Bluzy' },
-		{ path: 'category/accessories/1', name: 'Akcesoria' },
+		{ path: 'categories/t-shirts/1', name: 'T-Shirts' },
+		{ path: 'categories/hoodies/1', name: 'Bluzy' },
+		{ path: 'categories/accessories/1', name: 'Akcesoria' },
 		{ path: 'polityka-prywatnosci', name: '...' },
 		{ path: 'contact', name: '@' },
 	];
